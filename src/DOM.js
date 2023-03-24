7ecce2e021411f8b6252323701626b4381298fe0
yabets <yabets.g2@gmail.com>
2023-03-23 20:39:57 -0400

    Create buttons and apply more style

---
+++ src/DOM.js
@@ -0,0 +1,8 @@
+export const selectors = () => {
+  return {
+    addButton: document.querySelector(".addButton"),
+    cancelButton: document.querySelector(".cancelButton"),
+    addProjectButton: document.querySelector(".addProjectButton"),
+    addTaskButton: document.querySelector(".addTaskButton"),
+  };
+};
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { selectors } from "./DOM";
+
+
